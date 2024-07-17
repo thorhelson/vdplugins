ad80e9fe7d74cee21cd6193fe447b76c0bdb2ad8
thorhelson <thorhelson1@gmail.com>
2024-07-17 23:41:59 +0100

    testing

--- plugins/Dashless/src/index.ts
+++ plugins/Dashless/src/index.ts
@@ -1,20 +1,30 @@
 // https://github.com/ssense1337/enmity-plugins/blob/main/Dashless/src/index.tsx
-import { ReactNative } from "@vendetta/metro/common"
-import { after } from "@vendetta/patcher"
-import { findInReactTree } from "@vendetta/utils"
+import { ReactNative } from "@vendetta/metro/common";
+import { after } from "@vendetta/patcher";
+import { findInReactTree } from "@vendetta/utils";
 
-const { View } = ReactNative
+const { View } = ReactNative;
 
 const unpatch = after("render", View, (_: any, res: any) => {
-    const textChannel = findInReactTree(res, (r: any) => r?.props?.channel?.name && r?.props?.hasOwnProperty?.("isRulesChannel"))
-    if (!textChannel) return
-    after("type", textChannel.type, (_: any, res: any) => {
-        const textChannelName = findInReactTree(res, (r: any) => typeof r?.children === "string")
-        if (!textChannelName) return
-        textChannelName.children = textChannelName.children.replace(/-/g, " ")
-        return res
-    })
-    unpatch()
-})
+    try {
+        const textChannel = findInReactTree(res, (r: any) => r?.props?.channel?.name && r?.props?.hasOwnProperty?.("isRulesChannel"));
+        if (!textChannel) return;
 
-export const onUnload = () => unpatch()
+        after("type", textChannel.type, (_: any, res: any) => {
+            try {
+                const textChannelName = findInReactTree(res, (r: any) => typeof r?.children === "string");
+                if (!textChannelName) return;
+
+                textChannelName.children = textChannelName.children.replace(/-/g, " ");
+            } catch (error) {
+                console.error("Error processing textChannelName:", error);
+            }
+            return res;
+        });
+    } catch (error) {
+        console.error("Error processing textChannel:", error);
+    }
+    unpatch();
+});
+
+export const onUnload = () => unpatch();
