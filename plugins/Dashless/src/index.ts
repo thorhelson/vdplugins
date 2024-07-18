064ebdbdf07f4875433950c49003a7b99d3e98f8
thorhelson <thorhelson1@gmail.com>
2024-07-18 01:12:54 +0100

    Hopefully fixed Dashless

--- plugins/Dashless/src/index.ts
+++ plugins/Dashless/src/index.ts
@@ -1,30 +1,21 @@
-// https://github.com/ssense1337/enmity-plugins/blob/main/Dashless/src/index.tsx
 import { ReactNative } from "@vendetta/metro/common";
 import { after } from "@vendetta/patcher";
 import { findInReactTree } from "@vendetta/utils";
 
 const { View } = ReactNative;
 
-const unpatch = after("render", View, (_: any, res: any) => {
-    try {
-        const textChannel = findInReactTree(res, (r: any) => r?.props?.channel?.name && r?.props?.hasOwnProperty?.("isRulesChannel"));
-        if (!textChannel) return;
-
-        after("type", textChannel.type, (_: any, res: any) => {
-            try {
-                const textChannelName = findInReactTree(res, (r: any) => typeof r?.children === "string");
-                if (!textChannelName) return;
-
-                textChannelName.children = textChannelName.children.replace(/-/g, " ");
-            } catch (error) {
-                console.error("Error processing textChannelName:", error);
-            }
-            return res;
-        });
-    } catch (error) {
-        console.error("Error processing textChannel:", error);
-    }
-    unpatch();
+const unpatch = after("render", View, (_, res) => {
+    const anonymousForwardRefs = findInReactTree(res, r => (
+        typeof r?.props?.children === "string" && r.props.children.includes("-")
+    ));
+    
+    if (!anonymousForwardRefs) return;
+    
+    anonymousForwardRefs.forEach(anonymousRef => {
+        anonymousRef.props.children = anonymousRef.props.children.replace(/-/g, " ");
+    });
+    
+    return res; // Return the modified result
 });
 
 export const onUnload = () => unpatch();
